3d2fe989d14ce6bd4c263e2d05a09cc13cb1a6d0
Alexandr Petrenko <74877390+Alex34972@users.noreply.github.com>
2021-05-24 19:23:16 +0300

    Add files via upload

---
+++ delay.js
@@ -0,0 +1,19 @@
+const delay = (ms) => {
+  // Твой код
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const time = ms;
+      resolve(logger(time));
+    }, `${ms}`);
+  });
+};
+const logger = (time) => {
+  if (time !== undefined) {
+    console.log(`Resolved after ${time}ms`);
+  }
+};
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
